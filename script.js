ed71a7013c6345079512f15e6bba7db7d8b0a308
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-05-18 17:30:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // complete the given function
 
 function palindrome(str){
-
+  let lowerstr=str.toLowerCase();
+	let isPalindrome=true;
+	let i=0;
+	let j=lowerstr.length-1;
+	while(i<j){
+		if(lowerstr[i]!==lowerstr[j]){
+			isPalindrome=false;
+			break;
+		}
+		i++;
+		j--;
+	}
+	return isPalindrome;
 }
+
 module.exports = palindrome
